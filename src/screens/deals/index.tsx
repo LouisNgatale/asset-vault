c8e65a452d743f1673f3455b245992fece258f54
Louis Ngatale <louisngatale@gmail.com>
2024-06-03 13:25:01 +0300

    deal list card

--- src/screens/deals/index.tsx
+++ src/screens/deals/index.tsx
@@ -1,6 +1,26 @@
 import React from 'react';
-import { View } from 'react-native';
+import { SafeAreaView, ScrollView, TouchableOpacity, View } from 'react-native';
+import ThemeText from '../../components/theme-text.tsx';
+import tw from '../../lib/tailwind.ts';
 
 export default function Deals() {
-  return <View></View>;
+  return (
+    <SafeAreaView>
+      <ScrollView contentContainerStyle={tw`p-3`}>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((deal) => (
+          <TouchableOpacity
+            style={tw`w-full rounded-md bg-white mb-3 p-3 flex flex-row gap-3`}>
+            <View style={tw`h-[80px] w-[80px] bg-gray-200`}>
+              <ThemeText>Image view</ThemeText>
+            </View>
+            <View>
+              <ThemeText>Deal card</ThemeText>
+              <ThemeText type="subtext">Asset location description</ThemeText>
+              <ThemeText type="subtext">Asset valuation</ThemeText>
+            </View>
+          </TouchableOpacity>
+        ))}
+      </ScrollView>
+    </SafeAreaView>
+  );
 }
